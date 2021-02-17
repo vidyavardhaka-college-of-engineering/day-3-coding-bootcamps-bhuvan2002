f32521d55228b5516ec341e4d98e21a0f871be3d
BHUVAN U KADLAS <bhuvanumesh123@gmail.com>
2021-02-17 11:08:20 +0000

    Wrote a program to Check whether an integer is odd or even

--- Problem_3.c
+++ Problem_3.c
@@ -1,14 +1,21 @@
 // Check whether an integer is odd or even
 
 #include <stdio.h>
-int main() {
+int main()
+ {
     int number;
     printf("Enter an integer: ");
     scanf("%d", &number);
 
     // Write your code here
     
-    
-   
+    if(number% 2==0)
+    {
+      printf("Number is Even");
+    }
+   else
+   {
+     printf("Number is odd");
+   }
     return 0;
 }
