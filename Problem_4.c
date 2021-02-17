d0a20ecd19879fa937c9db7f0bb1a498a0836472
BHUVAN U KADLAS <bhuvanumesh123@gmail.com>
2021-02-17 11:44:02 +0000

    Wrote a program to print half pyramid of stars.

--- Problem_4.c
+++ Problem_4.c
@@ -1,16 +1,21 @@
 //Your task here is to print half pyramid of stars.
 
 #include<stdio.h>
-
-
 int main()
 {
-  	int n;
+  	int n,a,b;
 
   	scanf("%d", &n);
 
   	//Write your code her
-    
+    for(a=1;a<=n;a++)
+    {
+      for(b=1;b<=a;b++)
+      {
+        printf("*");
+      }
+      printf("\n");
+    }
 
   
 
